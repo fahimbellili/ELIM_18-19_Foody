e6649cce57a2584d583145eacb4e973bb26219a8
Fahim BELLILI <fahimbellili@gmail.com>
2019-01-23 10:57:45 +0100

    Adding Open Food Fact service class (Interface)

---
+++ FoodyApp/app/src/main/java/com/foody/foodyapp/OpenFoodFactService.java
@@ -0,0 +1,8 @@
+package com.foody.foodyapp;
+
+public interface OpenFoodFactService {
+
+    public static final String ENDPOINT = "https://fr.openfoodfacts.org/api/v0/produit/3029330003533.json";
+
+
+}
