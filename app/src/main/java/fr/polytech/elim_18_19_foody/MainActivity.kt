9c0dd4b1bd9dd56d93715aad7c88cefd24786d4a
Fahim BELLILI <fahimbellili@gmail.com>
2019-02-19 16:24:18 +0100

    Request permission method

--- app/src/main/java/fr/polytech/elim_18_19_foody/MainActivity.kt
+++ app/src/main/java/fr/polytech/elim_18_19_foody/MainActivity.kt
@@ -3,9 +3,13 @@ package fr.polytech.elim_18_19_foody
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityCompat
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.mindorks.paracamera.Camera
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,12 +35,41 @@ class MainActivity : AppCompatActivity() {
             .build(this)
     }
 
-
     private fun hasPermission(permission: String): Boolean {
         return ActivityCompat.checkSelfPermission(
             this,
             permission
         ) == PackageManager.PERMISSION_GRANTED
     }
-    
+
+    private fun requestPermissions() {
+        // TODO: provide an implementation
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) {
+
+            mainLayout.snack(getString(R.string.permission_message), Snackbar.LENGTH_INDEFINITE) {
+                action(getString(R.string.OK)) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            android.Manifest.permission.CAMERA
+                        ), PERMISSION_REQUEST_CODE
+                    )
+                }
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.CAMERA
+                ), PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+    }
 }
