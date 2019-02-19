65096b56d12eb7a91ab47b265e804e8bcc120c83
Tarik HAMLAT <hthamlat@gmail.com>
2019-02-19 15:17:16 +0100

    Init Firebase

--- app/src/main/java/fr/polytech/elim_18_19_foody/MainActivity.kt
+++ app/src/main/java/fr/polytech/elim_18_19_foody/MainActivity.kt
@@ -2,11 +2,15 @@ package fr.polytech.elim_18_19_foody
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Init Firebase
+        FirebaseApp.initializeApp(this)
     }
 }
