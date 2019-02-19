3fc48b9f76e987ce7b6ea011147ca771b413df99
Fahim BELLILI <fahimbellili@gmail.com>
2019-02-19 14:49:43 +0100

    Kotlin refactoring

---
+++ app/src/main/java/fr/polytech/elim_18_19_foody/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.polytech.elim_18_19_foody
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
